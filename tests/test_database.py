f9239c92b9859e58acea7deaab4846da1f657a89
apudovkin <ghord66@gmail.com>
2024-04-05 13:38:05 +0500

    fix database tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,4 +1,5 @@
 from praktikum.database import Database
+from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE
 
 
 class TestDatabase:
@@ -6,10 +7,14 @@ class TestDatabase:
         data = Database()
         data_buns = data.available_buns()
 
-        assert data_buns[0].get_name() == "black bun" and data_buns[0].get_price() == 100
+        assert data_buns[0].get_name() == 'black bun' and data_buns[0].get_price() == 100
 
     def test_available_ingredients_success_return(self):
         data = Database()
         data_ingredients = data.available_ingredients()
 
-        assert data_ingredients[0].get_name() == 'hot sauce' and data_ingredients[0].get_price() == 100
+        assert (
+                data_ingredients[0].get_type() == INGREDIENT_TYPE_SAUCE and
+                data_ingredients[0].get_name() == 'hot sauce' and
+                data_ingredients[0].get_price() == 100
+        )
